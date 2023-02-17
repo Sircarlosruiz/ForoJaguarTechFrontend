5416236b6934c98a86ab28cdd8be45cd06896853
Carlos Ruiz <carlosbustillo99@gmail.com>
2023-02-17 15:24:20 -0600

    changin in context of user

--- src/context/user.context.jsx
+++ src/context/user.context.jsx
@@ -1,45 +1,66 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+// import { useState, useEffect } from "react";
+// import axios from "axios";
 
-const UserContext = () => {
-  const token = window.localStorage.getItem("token");
-  const [isValid, setIsValid] = useState(null);
-  const [loggedUser, setLoggedUser] = useState({});
-  const [categories, setCategories] = useState([]);
-  const [catArray, setCatArray] = useState([]);
+import { createContext, useEffect, useState } from "react";
+
+// const UserContext = () => {
+//   const token = window.localStorage.getItem("token");
+//   const [isValid, setIsValid] = useState(null);
+//   const [loggedUser, setLoggedUser] = useState({});
+//   const [categories, setCategories] = useState([]);
+//   const [catArray, setCatArray] = useState([]);
+
+//   useEffect(() => {
+//     axios
+//       .get("http://localhost:8000/user/", {
+//         headers: {
+//           Authorization: token,
+//         },
+//       })
+//       .then(function (response) {
+//         const user = response.data.user_info;
+//         // const id = response.data.user_info.id
+//         setLoggedUser(user);
+//         setIsValid(true);
+//       })
+//       .catch(function (error) {
+//         console.log(error);
+//         window.localStorage.removeItem("token");
+//         window.localStorage.removeItem("isLoggedIn");
+//         setLoggedUser({});
+//         setIsValid(false);
+//       });
+
+//     // fetching all categories
+//     axios
+//       .get("http://localhost:8000/all-categories")
+//       .then(function (response) {
+//         setCategories(response.data);
+//       })
+//       .catch(function (error) {
+//         console.log(error);
+//       });
+//   }, [token]);
+//   return <></>;
+// };
+
+// export default UserContext;
+
+export const UserContext = createContext({
+  user: {},
+});
+
+export const UserProvider = ({ children }) => {
+  const token = window.localStorage.getItem("token")
+  const [user, setUser] = useState([]);
+  const value = { user };
+
 
   useEffect(() => {
-    axios
-      .get("http://localhost:8000/user/", {
-        headers: {
-          Authorization: token,
-        },
-      })
-      .then(function (response) {
-        const user = response.data.user_info;
-        // const id = response.data.user_info.id
-        setLoggedUser(user);
-        setIsValid(true);
-      })
-      .catch(function (error) {
-        console.log(error);
-        window.localStorage.removeItem("token");
-        window.localStorage.removeItem("isLoggedIn");
-        setLoggedUser({});
-        setIsValid(false);
-      });
+    const setUser = async() => {
+      const response = await fetch()
+    }
+  })
 
-    // fetching all categories
-    axios
-      .get("http://localhost:8000/all-categories")
-      .then(function (response) {
-        setCategories(response.data);
-      })
-      .catch(function (error) {
-        console.log(error);
-      });
-  }, [token]);
-  return <></>;
+  return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
-
-export default UserContext;
