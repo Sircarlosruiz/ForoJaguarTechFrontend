7e19e9ae7888af0d404c247f9e4fe67668da682d
Carlos Ruiz <carlosbustillo99@gmail.com>
2023-03-28 13:16:29 -0600

    fixing auth login

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import { useState } from 'react'
+import { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import Auth from "./routes/auth/auth.component";
 import Home from "./routes/home/Home";
 import NavigationHeader from "./routes/navigation/Navigation-Header.component";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(true)
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
     <Routes>
-      <Route exact path="/" element={isLoggedIn ? <NavigationHeader/> : null}>
-        <Route index element={isLoggedIn ? <Home/> : <Auth showSignIn/>} />
+      <Route exact path="/" element={isLoggedIn ? <NavigationHeader /> : null}>
+        <Route index element={isLoggedIn ? <Home /> : <Auth showSignIn />} />
         <Route path="/sign-up" element={<Auth showSignUp={setIsLoggedIn} />} />
         <Route path="/sign-in" element={<Auth showSignIn={setIsLoggedIn} />} />
       </Route>
