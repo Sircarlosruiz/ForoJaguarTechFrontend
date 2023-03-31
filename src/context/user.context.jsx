a615e50740932483af0f8c451dfd1b756a3546cf
Carlos Ruiz <carlosbustillo99@gmail.com>
2023-03-31 17:24:33 -0600

    [add] changes user context

--- src/context/user.context.jsx
+++ src/context/user.context.jsx
@@ -1,4 +1,8 @@
-import { createContext, useState } from "react";
+import { createContext, useState, useEffect } from "react";
+import {
+  createUserDocFromAuth,
+  onAuthStateChangedListener,
+} from "../utils/firebase/firebase.utils";
 
 export const UserContext = createContext({
   currentUser: null,
@@ -8,18 +12,21 @@ export const UserContext = createContext({
 export const UserProvider = ({ children }) => {
   const [currentUser, setCurrentUser] = useState(null);
 
-  // useEffect(() => {
+  useEffect(() => {
+    const unsubscribe = onAuthStateChangedListener((user) => {
+      if (user) {
+        createUserDocFromAuth(user);
+      }
+      setCurrentUser(user);
+    });
 
-  //   return () => {
-      
-  //   }
-  // }, [user])
-  
+    return unsubscribe;
+  }, []);
 
-  const value = ({
+  const value = {
     currentUser,
     setCurrentUser
-  })
+  };
 
   return <UserContext.Provider value={value}>{children}</UserContext.Provider>;
 };
