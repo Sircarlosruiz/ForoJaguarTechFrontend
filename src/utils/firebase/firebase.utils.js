265e2f038a1de51d038a6eb81928ccec391dce92
Carlos Ruiz <carlosbustillo99@gmail.com>
2023-03-30 23:07:11 -0600

    [add] config in firebase

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -1,6 +1,15 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  FacebookAuthProvider,
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  signOut,
+} from "firebase/auth";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -13,9 +22,23 @@ const firebaseConfig = {
   storageBucket: "jaguartech-15cdb.appspot.com",
   messagingSenderId: "477877606652",
   appId: "1:477877606652:web:22c3e33474c354b612df79",
-  measurementId: "G-BMJCFECQ3N"
+  measurementId: "G-BMJCFECQ3N",
 };
 
 // Initialize Firebase
 export const firebaseApp = initializeApp(firebaseConfig);
 export const analytics = getAnalytics(firebaseApp);
+
+
+const googleProvider = new GoogleAuthProvider();
+const facebookProvider = new FacebookAuthProvider();
+
+googleProvider.setCustomParameters({
+    prompt: "select_account",
+})
+
+// Auth Providers
+export const auth = getAuth(firebaseApp);
+export const signInUserWithGooglePopup = () => signInWithPopup(auth, googleProvider);
+export const signInUserWithFacebookPopup = () => signInWithPopup(auth, facebookProvider);
+
