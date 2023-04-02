328f6781eb461d4d728f4d45cb68a8eddfe405a2
Carlos Ruiz <carlosbustillo99@gmail.com>
2023-04-01 21:30:54 -0600

    [add] changes in style in navigation

--- src/routes/navigation/Navigation-Header.component.jsx
+++ src/routes/navigation/Navigation-Header.component.jsx
@@ -1,4 +1,4 @@
-import { useCallback, useContext, useEffect, useState } from "react";
+import { useCallback, useContext, useEffect, useRef, useState } from "react";
 // import logo from '../assets/logos/logo.jpg'
 import { Outlet } from "react-router-dom";
 import {
@@ -12,10 +12,13 @@ import { MdWorkspacesFilled } from "react-icons/md";
 import { IoMdNotifications } from "react-icons/io";
 import { UserContext } from "../../context/user.context";
 
-import "./Navigation-Header.component.scss";
+import ModalContainer from "../../components/modal-container/modal-container.component";
 import UserIcon from "../../components/user-icon/user.icon.component";
 import UserMenu from "../../components/user-nav-menu/user-nav-menu.component";
 import { logOutUser } from "../../utils/firebase/firebase.utils";
+import { useOnClickOutside } from "../../components/utils/helpers";
+
+import "./Navigation-Header.component.scss";
 
 const NavigationHeader = () => {
   const [showSignInModal, setShowSignInModal] = useState(false);
@@ -25,19 +28,27 @@ const NavigationHeader = () => {
 
   console.log(currentUser);
 
+  const signInModalRef = useRef();
+
   //Effects
   useEffect(() => {
     currentUser && setShowSignInModal(false);
   }, [currentUser]);
 
   //callbacks functions
-  const showMenu = useCallback(() => {
-    setShowUserMenu(true)
-  }, [])
-
   const hideMenu = useCallback(() => {
-    setTimeout(() => setShowUserMenu(false), 600)
-  });
+    setTimeout(() => setShowUserMenu(false), 600);
+  }, []);
+
+  const showMenu = useCallback(() => {
+    setShowUserMenu(true);
+  }, []);
+
+  const hideModal = useCallback(({ target }) => {
+    //Do nothing If the click comes from any link in nav component
+    if (target.closest(".link")) return;
+    setShowSignInModal(false);
+  }, []);
 
   //Event handlers
   const handleLogOutUser = async () => {
@@ -48,6 +59,7 @@ const NavigationHeader = () => {
     }
   };
 
+  useOnClickOutside(signInModalRef, hideModal);
   return (
     <div className="navigation">
       <div className="menu-container">
@@ -74,25 +86,34 @@ const NavigationHeader = () => {
         </div>
 
         {!currentUser ? (
-          <div className="icon-user">
+          <div
+            onClick={() => setShowSignInModal(!showSignInModal)}
+            className="icon-user"
+          >
             <AiOutlineUser />
+            <p>Cuenta</p>
           </div>
         ) : (
           <>
             <UserIcon user={currentUser} showMenu={showMenu} />
             <UserMenu
               show={showUserMenu}
-              logOutUser={handleLogOutUser}
-              hideMenu={hideMenu}
               user={currentUser}
+              hideMenu={hideMenu}
+              logOutUser={handleLogOutUser}
             />
           </>
         )}
 
         <div className="add-question">
-          <label className="text-question">Añade tu pregunta</label>
           <AiOutlineQuestion className="icon-question-mark" />
+          <label className="text-question">Añade tu pregunta</label>
         </div>
+
+        <ModalContainer
+          show={showSignInModal}
+          className={"modal-container signIn"}
+        />
       </div>
       <Outlet />
     </div>
